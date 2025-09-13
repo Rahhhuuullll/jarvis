4ee7bbbb5e9ebd8b8b2328508f7f98c62e01c4bd
rahul <rahuldn1011@gmail.com>
2025-09-13 11:09:21 +0530

    hood design done

---
+++ www/main.js
@@ -0,0 +1,12 @@
+$(document).ready(function () {
+    $('.text').texillate({
+        loop: true,
+        sync: true,
+        in: {
+            effect: "bounceIn",
+        },
+        out: {
+            effect: "bounceOut",
+        }
+    });
+});
